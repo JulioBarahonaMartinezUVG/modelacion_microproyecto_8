3810b535d9ed4bd80527efc95738dc4e19710691
bar141206@uvg.edu.gt <bar141206@uvg.edu.gt>
2020-11-03 12:39:34 -0600

    estados  markov

--- micro_8.py
+++ micro_8.py
@@ -5,10 +5,33 @@ con transiciones estacionarias y número de estados finitos dado
 un estado inicial, muestre los diferentes estados hasta la 
 generación del estado en el paso k.
 """
+import numpy as np
+import random as rm
 
-def markovChain():
+def markovChain(n):
 	print('empieza cadena markov')
+
+	# Estados del informatico
+	states = ["Eat","Sleep","Code", "Repeat"]
+
+	# Possible sequences of events
+	transitionName = [
+		["EE","ES","EC","ER"],
+		["SE","SS","SC","SR"],
+		["CE","CS","CC","CR"],
+		["RE","RS","RC","RR"]
+	]
+
+	# Probabilities matrix (transition matrix)
+	transitionMatrix = [
+		[0.2,0.6,0.2],
+		[0.1,0.6,0.3],
+		[0.1,0.6,0.3],
+		[0.2,0.7,0.1]
+	]
+
+
 	print('termina cadena markov')
 
 
-markovChain()
+markovChain(10)
