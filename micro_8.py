67e8e38a318d50fbc1158ec86cdb3d9f33854585
bar141206@uvg.edu.gt <bar141206@uvg.edu.gt>
2020-11-03 10:58:19 -0600

    empiza markov

---
+++ micro_8.py
@@ -0,0 +1,14 @@
+"""
+Construya un programa de simulación, en su lenguaje favorito, 
+para generar el estado en el paso k de una cadena de Markov  
+con transiciones estacionarias y número de estados finitos dado 
+un estado inicial, muestre los diferentes estados hasta la 
+generación del estado en el paso k.
+"""
+
+def markovChain():
+	print('empieza cadena markov')
+	print('termina cadena markov')
+
+
+markovChain()
