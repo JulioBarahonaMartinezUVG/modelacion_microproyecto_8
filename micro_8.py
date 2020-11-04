477cf81ac467289ba182818d4913b5b2a85f1adf
bar141206@uvg.edu.gt <bar141206@uvg.edu.gt>
2020-11-04 14:28:08 -0600

    ej 2 done

--- micro_8.py
+++ micro_8.py
@@ -9,6 +9,15 @@ import pprint
 import numpy as np
 pp = pprint.PrettyPrinter(indent=4)
 
+def multMatrices(X,Y):
+	result = [[0,0,0,0]]	
+	for i in range(len(X)):
+	   for j in range(len(Y[0])):
+	       for k in range(len(Y)):
+	           result[i][j] += X[i][k] * Y[k][j]
+
+	print(result)
+
 def markovChain(n):
 	print('empieza cadena markov')
 
@@ -37,13 +46,70 @@ def markovChain(n):
 			T_new = I * T
 		else:
 			T_new = T_new * T
-
-		#print(T_new)
-	
-
-	print(T_new)
+			
+		print(T_new)
 
 	print('termina cadena markov')
+	
+
+	return(T_new)
 
 
-markovChain(n = 10)
+ej1 = markovChain(n = 10)
+print('Resultado final: \\n',ej1)
+
+
+"""
+Utilice el algoritmo de Hastings-Metrópolis para generar los 
+valores de una variable normal estándar, sea q(x,y) definido 
+como y=x+e donde los valores de e se generan de una variable 
+aleatoria G con g función de densidad de probabilidad simétrica 
+en 0. Además, q(x,y)=g(e).Realice el histograma con los 10000 valores resultantes.
+"""
+
+import numpy as np
+import math
+from numpy import linalg as la
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import time
+
+mu,sig,N = 0,1,1000000
+pts = []
+
+def q(x):
+    return (1/(math.sqrt(2*math.pi*sig**2)))*(math.e**(-((x-mu)**2)/(2*sig**2)))
+
+def metropolis(N):
+    r = np.zeros(1)
+    p = q(r[0])
+    pts = []
+    
+    for i in range(N):
+        rn = r + np.random.uniform(-1,1)
+        pn = q(rn[0])
+        if pn >= p:
+            p = pn
+            r = rn
+        else:
+            u = np.random.rand()
+            if u < pn/p:
+                p = pn
+                r = rn
+        pts.append(r)
+    
+    pts = np.array(pts)
+    return pts
+    
+def hist_plot(array):
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1,)
+    ax.hist(array, bins=1000)    
+    plt.title('')
+    plt.xlabel('')
+    plt.ylabel('')
+    plt.savefig('ej2.png')
+
+x = metropolis(10000)
+
+hist_plot(x)    
